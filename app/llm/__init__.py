0f2f973d423661e24d54051c992e9713d14e7537
blackstar <164309670+raven-book@users.noreply.github.com>
2025-05-15 06:41:22 +0000

    chore(api): add one-api class with empty method

--- app/llm/__init__.py
+++ app/llm/__init__.py
@@ -1,4 +1,15 @@
-
 class OneApi:
     def __init__(self, config):
         pass
+
+    async def forward(self, request):
+        """ 流式转发 """
+        # TODO
+
+    async def streaming(self, request):
+        """ 流式请求 """
+        # TODO
+
+    async def chat(self, request):
+        """ 普通请求 """
+        # TODO
